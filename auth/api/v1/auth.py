82684d340a4d77e21602999354f5997a7349c289
Федор Зоркий <fedor.zorky@yandex.ru>
2024-01-19 01:12:38 +0300

    Добавлены методы для работы с профилем и получения истории входов.

--- auth/api/v1/auth.py
+++ auth/api/v1/auth.py
@@ -5,8 +5,8 @@ from http import HTTPStatus
 from fastapi.security import HTTPBearer
 
 from db.redisdb import get_redis
-from schemas.user import UserCreated, UserCreateRequest, UserAuthRequest, JWTAccessToken, UserUpdateRequest, \
-    RevokedSessions, CreatedSession, RevokedTokens, UpdatedProfileFields, SessionsHistory
+from schemas.user import UserId, UserProfile, UserCredentials, JWTAccessToken, \
+    RevokedSessions, NewSession, RevokedTokens, UpdatedProfileFields, UserAttributes, SessionRecord
 from services.auth import AuthService, get_auth_service
 
 router = APIRouter(redirect_slashes=False, prefix="/auth", tags=['Auth'])
@@ -18,13 +18,13 @@ async def check_if_token_in_denylist(decrypted_token):
 
 
 @router.post('/signup', status_code=HTTPStatus.CREATED)
-async def create_user(request: UserCreateRequest, auth: AuthService = Depends(get_auth_service)) -> UserCreated:
+async def create_user(request: UserProfile, auth: AuthService = Depends(get_auth_service)) -> UserId:
     """Регистрация нового пользователя"""
     return await auth.create_user(request)
 
 
 @router.post("/login")
-async def login(request: UserAuthRequest, auth: AuthService = Depends(get_auth_service)) -> CreatedSession:
+async def login(request: UserCredentials, auth: AuthService = Depends(get_auth_service)) -> NewSession:
     """Аутентификация пользователя и создание новой сессии"""
     return await auth.authenticate(**request.model_dump())
 
@@ -54,12 +54,18 @@ async def access_revoke(auth: AuthService = Depends(get_auth_service)) -> Revoke
 
 
 @router.patch("/profile", dependencies=[Depends(HTTPBearer())])
-async def profile(request: UserUpdateRequest, auth: AuthService = Depends(get_auth_service)) -> UpdatedProfileFields:
+async def profile(request: UserCredentials, auth: AuthService = Depends(get_auth_service)) -> UpdatedProfileFields:
     """Обновление профилья пользователя"""
     return await auth.update_profile(request)
 
 
+@router.get("/profile", dependencies=[Depends(HTTPBearer())])
+async def profile(auth: AuthService = Depends(get_auth_service)) -> UserAttributes:
+    """Обновление профилья пользователя"""
+    return await auth.get_profile()
+
+
 @router.get("/history", dependencies=[Depends(HTTPBearer())])
-async def history(auth: AuthService = Depends(get_auth_service)) -> SessionsHistory:
+async def history(auth: AuthService = Depends(get_auth_service)) -> list[SessionRecord]:
     """Получение истории входов пользователя"""
     return await auth.get_history()
